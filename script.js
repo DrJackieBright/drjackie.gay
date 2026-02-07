08a3964262e256ea13e42c47540c86964cb60a08
DrJackieBright <brightestjackie@gmail.com>
2026-02-07 16:47:47 -0500

    I need a backend api

--- script.js
+++ script.js
@@ -1,36 +1,33 @@
-function fillBlinkies(elementID, path, height=50, width=100) {
-    const extensions = ["png", "gif", "jpg"]
-    const threads = Math.floor(navigator.hardwareConcurrency / 2)
-    var knownLimit = Infinity
-    for (let index = 1; index <= threads; index++) {
-        tryBlinky(index,threads,0)   
-    }
-    async function tryBlinky(index, jumpSize, ext_index) {
-        if (index > knownLimit) { return }     
-        // console.log(index, knownLimit, ext_index)
-        let test_url = `${path}/${index}.${extensions[ext_index]}`
-        const http = new XMLHttpRequest();
-        http.open('HEAD', test_url, true);
-        http.send();
-        http.onload = () => {
-            // console.log(http);
-            if (http.status!=404) {
-                var img = document.createElement("img")
-                img.height = height
-                img.width = width
-                img.src = http.responseURL
-                document.getElementById(elementID).appendChild(img)
-                tryBlinky(index + jumpSize, jumpSize, 0)
-            } else {
-                if (ext_index < extensions.length - 1) {
-                    tryBlinky(index, jumpSize, ext_index + 1)
-                } else {
-                    if (index < knownLimit) {
-                        // console.log(index)
-                        knownLimit = index
-                    }
-                }
-            }
+var apiRoot = "http://localhost:8888"
+
+function fillBlinkies(elementID, type, height=50, width=100) {
+    const http = new XMLHttpRequest()
+    http.open("GET", `${apiRoot}/blinkies`)
+    http.send()
+    http.onload = () => {
+        let listing = JSON.parse(http.response)
+        listing[type].sort(() => Math.random() - 0.5);
+        for (const blinkie of listing[type]) {
+            var img = document.createElement("img")
+            img.height = height
+            img.width = width
+            img.src = blinkie
+            document.getElementById(elementID).appendChild(img)
         }
+    }
+}
+
+function spotifyEmbed() {
+    const http = new XMLHttpRequest()
+    http.open("GET", `${apiRoot}/lastscrobble`)
+    http.send()
+    http.onload = () => {
+        let track = JSON.parse(http.response)
+        document.getElementById("spotify-embed").src = document.getElementById("spotify-embed").src.replace("4PTG3Z6ehGkBFwjybzWkR8", track.spotify_track_ids[0])
+        document.getElementById("lastfm-title").innerText = track.name
+        document.getElementById("lastfm-title").href = track.url
+        document.getElementById("lastfm-artist").innerText = track.artist["#text"]
+        document.getElementById("lastfm-album").innerText = track.album["#text"]
+        document.getElementById("lastfm-cover").src = track.image[track.image.length - 1]["#text"]
     }
 }
